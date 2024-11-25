543c639b77c7336e4a43b0c551c3f4a86d20d621
ahmad-ebrahim-1 <ahmadeb911@gmail.com>
2024-11-25 14:48:14 +0300

    update animation

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -10,8 +10,8 @@ const Rocket = (props) => {
   useGSAP(() => {
     if (rocketRef.current && rocketRef.current.position)
       gsap.from(rocketRef.current.position, {
-        x: rocketRef.current.position.x - 10,
-        z: rocketRef.current.position.z + 10,
+        x: rocketRef.current.position.x - 5,
+        z: rocketRef.current.position.z + 5,
         duration: 2,
       });
   });
